9f73425628e74bbd0efd2ac94de7a44adad84edb
malindads <malindads2020@gmail.com>
2025-06-29 11:48:14 +0530

    modified:   src/pages/ApplicationForm.jsx

--- src/pages/ApplicationForm.jsx
+++ src/pages/ApplicationForm.jsx
@@ -173,8 +173,8 @@ export default function ApplicationForm() {
                       <option value="">Select Gender</option>
                       <option value="male">Male</option>
                       <option value="female">Female</option>
-                      <option value="other">Other</option>
-                      <option value="prefer-not-to-say">Prefer not to say</option>
+
+
                     </select>
                   </div>
 
