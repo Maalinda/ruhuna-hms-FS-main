f13eaf69f56743dba2695c1a4d5c6d7031471890
ImeshMalinda <imeshmalinda8626@gmail.com>
2025-08-29 22:35:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ApplicationForm from "./pages/ApplicationForm"
 import AdminDashboard from "./pages/AdminDashboard"
 import StudentDashboard from "./pages/StudentDashboard"
 import NotFound from "./pages/NotFound"
+import AboutUs from "./pages/AboutUs"
 
 function App() {
   return (
@@ -47,6 +48,7 @@ function App() {
                   </PrivateRoute>
                 }
               />
+              <Route path="/aboutus" element={<AboutUs />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
